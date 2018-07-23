30adec8289d0481d43bb7f1c6a5b164eb4f61ca9
Suho Oh <berlinsuho@gmail.com>
2018-07-23 21:06:45 +0200

    Add little fixes

--- SimonSays_Memory/ViewController.swift
+++ SimonSays_Memory/ViewController.swift
@@ -1,9 +1,13 @@
 import UIKit
 import AVFoundation
 
+let kDelayBetweenStages = 0.75
+let kPlayDuration = 0.4
+let kHighScoreKey = "HighScore"
+
 class ViewController: UIViewController {
 
-  @IBOutlet weak var highestStageLabel: UILabel!
+  @IBOutlet weak var highScoreLabel: UILabel!
 
   @IBOutlet weak var btn0: UIButton!
   @IBOutlet weak var btn1: UIButton!
@@ -11,75 +15,165 @@ class ViewController: UIViewController {
   @IBOutlet weak var btn3: UIButton!
   @IBOutlet weak var startBtn: UIButton!
 
-  var userDefault = UserDefaults.standard
+  let userDefault = UserDefaults.standard
   var correctAnswers: [Int] = []
   var userInputs: [Int] = []
-  var flashedIdx = 0
-  var tappedCountIdx = 0
+  var playedIdx = 0
+  var inputIdx = 0
   var stage = 0
-  var highestStageStage = 0
+  var highScore = 0
 
   var audioPlayer: AVAudioPlayer = AVAudioPlayer()
-  var timer = Timer()
-  var remainingSeconds: Float = 10
+//  var timer = Timer()
+//  var remainingSeconds: Float = 10
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    loadHighScore()
+    enableAllBtns(false)
+  }
+
+  func loadHighScore() {
+    highScore = userDefault.integer(forKey: kHighScoreKey)
+    highScoreLabel.text = "High score: \(highScore)"
+  }
 
   @IBAction func startBtnTapped(_ sender: UIButton) {
-    correctAnswers.removeAll()
-    userInputs.removeAll()
-    flashedIdx = 0
-    stage = 0
+    newGame()
     nextStage()
   }
 
-  func nextStage() {
-    tappedCountIdx = 0
+  func newGame() {
+    correctAnswers.removeAll()
+    playedIdx = 0
+    stage = 0
+    clearUserInputs()
+  }
+
+  func clearUserInputs() {
     userInputs.removeAll()
-    enableAllBtns(false)
+    inputIdx = 0
+  }
+
+  func nextStage() {
+    clearUserInputs()
     correctAnswers.append(Int(arc4random_uniform(4)))
     print("correctAnswer \(correctAnswers)")
 
-    flashedIdx = 0
-    stage += 1
-    startBtn.setTitle("\(stage)", for: .normal)
-    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + kDelayBetweenStages) {
+      self.stage += 1
+      self.startBtn.setTitle("\(self.stage)", for: .normal)
       self.playSound(soundName: "upNextStage")
     }
-    DispatchQueue.main.asyncAfter(deadline: .now() + 0.9) {
-      self.playBtnFlashAutomatically()
+
+    playedIdx = 0
+    enableAllBtns(false)
+    DispatchQueue.main.asyncAfter(deadline: .now() + (kDelayBetweenStages + 1.0)) {
+      self.playAnswer()
     }
   }
 
-  func enableAllBtns(_ enabled: Bool) {
-    btn0.isEnabled = enabled
-    btn1.isEnabled = enabled
-    btn2.isEnabled = enabled
-    btn3.isEnabled = enabled
-  }
-
-  func playBtnFlashAutomatically() {
-    //    userInputs = correctAnswers
-    if correctAnswers.count <= flashedIdx {
-      flashedIdx = 0
+  func playAnswer() {
+    guard playedIdx < correctAnswers.count else {
+      playedIdx = 0
       enableAllBtns(true)
       return
     }
 
-    let intFromArray = correctAnswers[flashedIdx]
-    playBtnFlash(btn: convertIntToBtn(from: intFromArray), completion: {_ in
-      self.playSound(soundName: "sound\(intFromArray)")
-      self.flashedIdx += 1
-      self.playBtnFlashAutomatically()
-    })
+    let answer = correctAnswers[playedIdx]
+    let btn = btnFromAnswer(answer)
+    flashBtn(btn) {_ in
+      self.playedIdx += 1
+      self.playAnswer()
+    }
   }
 
-  func playBtnFlash(btn: UIButton, completion: ((Bool) -> Void)? = nil) {
+  func flashBtn(_ btn: UIButton, completion: ((Bool) -> Void)? = nil) {
     btn.alpha = 0.3
-    UIView.animate(withDuration: 0.7, delay: 0.0, options: .curveEaseInOut, animations: {
-      btn.alpha = 1
-    }, completion: completion)
+    let answer = answerFromBtn(btn)
+    playSound(soundName: "sound\(answer)")
+    UIView.animate(
+      withDuration: kPlayDuration,
+      delay: 0.0,
+      options: .curveEaseInOut,
+      animations: {
+        btn.alpha = 1
+      },
+      completion: completion
+    )
   }
 
-  func convertIntToBtn(from: Int) -> UIButton {
+//  func runTimer() {
+//    timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: (#selector(ViewController.timeLimit)), userInfo: nil, repeats: true)
+//  }
+//
+//  @objc func timeLimit() {
+////    timeProgressBar.setProgress(Float(remainingSeconds)/100.0, animated: true)
+//    if remainingSeconds != 0 {
+//      remainingSeconds -= 1
+//    } else {
+//      timer.invalidate()
+//      endGame()
+//    }
+//  }
+
+  @IBAction func btnDown(_ sender: UIButton) {
+    let guess = answerFromBtn(sender)
+    self.playSound(soundName: "sound\(guess)")
+    sender.alpha = 0.3
+  }
+
+  @IBAction func btnUp(_ sender: UIButton) {
+    let guess = answerFromBtn(sender)
+    userInputs.append(guess)
+    print("userInputs: \(userInputs)")
+
+    if guess == correctAnswers[inputIdx] {
+      inputIdx += 1
+      if userInputs.count == correctAnswers.count {
+        nextStage()
+      }
+    } else {
+      endGame()
+    }
+
+    sender.alpha = 1
+  }
+
+  func endGame() {
+    enableAllBtns(false)
+    playSound(soundName: "gameOver")
+    // timer.invalidate()
+
+    let finalScore = stage - 1
+    let highestScore = finalScore > highScore ? finalScore : highScore
+    highScore = highestScore
+    startBtn.setTitle("\(finalScore)", for: .normal)
+
+    userDefault.set(highestScore, forKey: kHighScoreKey)
+    userDefault.synchronize()
+    loadHighScore()
+
+    print("gameEnd")
+  }
+
+  func playSound(soundName: String) {
+    let audioPath = Bundle.main.path(forResource: soundName, ofType: "wav", inDirectory: "audio")!
+    let url = URL(fileURLWithPath: audioPath, isDirectory: true)
+
+    do {
+      audioPlayer = try AVAudioPlayer(contentsOf: url)
+    } catch {
+      print("nooooo..")
+    }
+    audioPlayer.play()
+  }
+
+  func answerFromBtn(_ from: UIButton) -> Int {
+    return from.tag
+  }
+
+  func btnFromAnswer(_ from: Int) -> UIButton {
     switch from {
     case 0:
       return btn0
@@ -94,78 +188,10 @@ class ViewController: UIViewController {
     }
   }
 
-  //  func runTimer() {
-  //    timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: (#selector(ViewController.timeLimit)), userInfo: nil, repeats: true)
-  //  }
-  //
-  //  @objc func timeLimit() {
-  ////    timeProgressBar.setProgress(Float(remainingSeconds)/100.0, animated: true)
-  //    if remainingSeconds != 0 {
-  //      remainingSeconds -= 1
-  //    } else {
-  //      timer.invalidate()
-  //      endGame()
-  //    }
-  //  }
-
-  func endGame() {
-    playSound(soundName: "gameOver")
-    //    timer.invalidate()
-    correctAnswers.removeAll()
-    userInputs.removeAll()
-    let finalScore = stage - 1
-    startBtn.setTitle("\(finalScore)", for: .normal)
-
-    var highestStage = 0
-    highestStage = highestStage > finalScore ? highestStage : finalScore
-    userDefault.set(highestStage, forKey: "BestScore")
-    highestStageLabel.text = "BEST  \(highestStage)"
-    
-    highestStageStage = userDefault.integer(forKey: "BestScore")
-    stage = 0
-    print("gameEnd")
-  }
-
-  @IBAction func tapBtn(_ sender: UIButton) {
-    sender.flash()
-    playSound(soundName: "sound\(sender.tag)")
-    userInputs.append(sender.tag)
-    print("userInputs: \(userInputs)")
-
-    if sender.tag == correctAnswers[tappedCountIdx] {
-      tappedCountIdx += 1
-      if userInputs.count == correctAnswers.count {
-        tappedCountIdx = 0
-        enableAllBtns(false)
-        nextStage()
-      }
-    } else if sender.tag != correctAnswers[tappedCountIdx] {
-      tappedCountIdx = 0
-      endGame()
-    }
-  }
-
-  func playSound(soundName: String) {
-    let audioPath = Bundle.main.path(forResource: soundName, ofType: "wav", inDirectory: "audio")!
-    let url = URL(fileURLWithPath: audioPath, isDirectory: true)
-
-    do {
-      audioPlayer = try AVAudioPlayer(contentsOf: url)
-    } catch {
-      print("nooooo..")
-    }
-    audioPlayer.play()
-  }
-}
-
-extension UIButton {
-  func flash() {
-    let flash = CABasicAnimation(keyPath: "opacity")
-    flash.duration = 0.1
-    flash.fromValue = 1
-    flash.toValue = 0.1
-    flash.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-
-    layer.add(flash, forKey: nil)
+  func enableAllBtns(_ enabled: Bool) {
+    btn0.isEnabled = enabled
+    btn1.isEnabled = enabled
+    btn2.isEnabled = enabled
+    btn3.isEnabled = enabled
   }
 }
